2fd6fe14cf07c392e21988f7268f6905c1a7ca4b
mogomotsi@next45.co <mogomotsimodise@Mogomotsis-MacBook-Pro.local>
2021-04-08 13:40:27 +0200

    rename test methods and file formating

--- test/controllers/static_pages_controller_test.rb
+++ test/controllers/static_pages_controller_test.rb
@@ -6,31 +6,31 @@ class StaticPagesControllerTest < ActionDispatch::IntegrationTest
     @base_title = "Ruby on Rails Tutorial Sample App"
   end
   
-  test "should get home" do
+  test "GET #home renders the home page" do
     get static_pages_home_url
     assert_response :success
     assert_select "title", "Home | #{@base_title}"
   end
 
-  test "should get help" do
+  test "GET #help renders the help page" do
     get static_pages_help_url
     assert_response :success
     assert_select "title", "Help | #{@base_title}"
   end
 
-  test "should get about" do
+  test "GET #home renders the about page" do
     get static_pages_about_url
     assert_response :success
     assert_select "title", "About | #{@base_title}"
   end
 
-  test "should get contact" do
+  test "GET #home renders the contact page" do
     get static_pages_contact_url
     assert_response :success
     assert_select "title", "Contact | #{@base_title}"
   end
 
-  test "should get root" do
+  test "GET with the root url renders the home page" do
     get root_url
     assert_response :success
   end
