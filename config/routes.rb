c618863f0f2eb1630f3458dba9396492bfb3f368
mogomotsi@next45.co <mogomotsimodise@Mogomotsis-MacBook-Pro.local>
2021-04-22 16:23:21 +0200

    Complete chapter8

--- config/routes.rb
+++ config/routes.rb
@@ -7,7 +7,7 @@ Rails.application.routes.draw do
   get '/signup', to: 'users#new'
   get '/login', to: "sessions#new"
   post '/login', to: "sessions#create"
-  delete '/login', to: 'session#destroy'
+  delete '/logout', to: 'sessions#destroy'
   resources :microposts
   resources :users
 end
