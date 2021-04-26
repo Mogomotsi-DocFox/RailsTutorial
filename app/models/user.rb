334057774b091289b2d1334f9db8f6dc62fd2308
mogomotsi@next45.co <mogomotsimodise@Mogomotsis-MBP.home>
2021-04-26 08:50:23 +0200

    Add user digest to users

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,5 @@
 class User < ApplicationRecord
+    attr_accessor :remember_token
     has_many :microposts
     before_save { self.email = email.downcase }
     validates :name, presence: true, length: { maximum: 50 }
@@ -8,4 +9,13 @@ class User < ApplicationRecord
                     uniqueness: true
     has_secure_password
     validates :password, presence: true, length: { minimum: 6}
+
+    def User.new_token
+        SecureRandom.urlsafe_base64
+    end
+
+    def remember
+        self.remember_token = User.new_token
+        update_attribute(:remember_digest, User.digest(remember_token))
+    end
 end
